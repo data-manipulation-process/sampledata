b899e5798f131804a72b1461bf8180cbdd0c97bd
data-manipulation-process <jagadusu01@gmail.com>
2023-02-19 19:56:24 +0000

    list updated in the class-4

---
+++ class-4/list.py
@@ -0,0 +1,29 @@
+
+
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits[1])
+
+fruits[0] = "kiwi"
+
+print(fruits)
+
+fruits.append("orange")
+
+print(fruits)
+
+fruits.insert(1,"lemon")
+
+print(fruits)
+
+fruits.remove("banana")
+
+print(fruits)
+
+print(fruits[-1])
+
+print(fruits[2:5])
+
+print(len(fruits))
+
+
