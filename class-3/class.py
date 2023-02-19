1179b0a575990c0ce6497c1902948d1b3958352f
data-manipulation-process <jagadusu01@gmail.com>
2023-02-19 13:51:22 +0000

    iterator updated

---
+++ class-3/class.py
@@ -0,0 +1,17 @@
+"""
+class example
+"""
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.lastname, self.lastname)
+
+x = Person("Jagan", "Naidu")
+
+
+mytuple = ("apple", "banna", "cherry")
+
