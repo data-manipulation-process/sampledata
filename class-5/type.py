f1f1a3ed3f9ed03f2bb13bbbf211f505811d6ecf
data-manipulation-process <jagadusu01@gmail.com>
2023-02-19 20:40:00 +0000

    boolean, number, string, type updated

---
+++ class-5/type.py
@@ -0,0 +1,27 @@
+
+
+x = 5
+
+print(type(x))
+
+y = "Hello World"
+
+print(type(y))
+
+z = 20.5
+
+print(type(z))
+
+A = ["apple", "banana", "cherry"]
+
+print(type(A))
+
+B = ("apple", "banana", "cherry")
+
+print(type(B))
+
+c = {"name" : "John", "age" : 36 }
+print(type(c))
+
+D = True
+print(type(D))
