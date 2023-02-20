8bd4bf31c832c0539cb1a9220e064118e16aa720
data-manipulation-process <jagadusu01@gmail.com>
2023-02-20 16:24:35 +0000

    class-7 append file runners updated

---
+++ class-7/appendingfile.py
@@ -0,0 +1,14 @@
+file1 = open("myfile.txt","w")
+L = ["This is Delhi \n","This is Paris \n","This is London \n"]
+
+file1.writelines(L)
+file1.close()
+
+
+with open("myfile.txt", 'a') as f:
+    f.write('End of the line\n')
+
+with open("myfile.txt", 'r') as f:
+    for i, lines in enumerate(f):
+        print(lines.strip())
+
