2c22f6a2754ef2abb548eac4ecb6c48169920d26
data-manipulation-process <jagadusu01@gmail.com>
2023-02-20 12:14:58 +0000

    file reader \n\n to \n dos2unix

--- class-6/readfile.py
+++ class-6/readfile.py
@@ -1,4 +1,8 @@
 import os
+import re
+
+import numpy as np
+import pandas as pd
 
 fDir = "/Users/developer1/workspace/sampledata/class-6"
 fFile = "newslines.txt"
@@ -20,8 +24,20 @@ print(fPath)
 # with open(fPath, 'r') as reader:
 #     for line in reader:
 #         print(line, end='')
+
 with open(fPath, 'r') as reader:
     line1 = reader.readlines()
+
 with open(fPath, 'w') as writer:
     for line in reversed(line1):
-        writer.write(line)
+        if not line.isspace():
+            writer.write(line)
+            print(line, end='')
+
+
+# with open(fPath, 'r') as reader:
+#     for line in reader:
+#         if not line.isspace():
+#             print(line, end='')
+
+# Add n blank rows
---
+++ class-6/workingwithbytes.py
@@ -0,0 +1,21 @@
+import os
+
+fDir = "/Users/developer1/workspace/sampledata/class-6"
+fFile = "newslines.txt"
+
+fPath = os.path.join(fDir, fFile)
+
+print(fPath)
+
+# with open(fPath, 'r') as reader:
+#     line1 = reader.readlines()
+#
+# with open(fPath, 'w') as writer:
+#     for line in reversed(line1):
+#         if not line.isspace():
+#             writer.write(line)
+#             print(line, end='')
+with open (fPath, 'rb') as reader:
+    print(reader.read(10))
+    print(reader.read(3))
+    print(reader.read(2))
