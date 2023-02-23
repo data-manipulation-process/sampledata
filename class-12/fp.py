0e7439233c781d43ac602a73c5b5ce19b135f314
data-manipulation-process <jagadusu01@gmail.com>
2023-02-23 23:49:56 +0000

    search string in the file and print line with number

--- class-12/fp.py
+++ class-12/fp.py
@@ -1,27 +1,32 @@
 
 import os
+import re
 
 dirpath = "/Users/developer1/workspace/sampledata"
-
-
-# text = input("Please enter text: " )
-#
-# print(text)
-
-search_string = 'test'
-
-import os
 
 import glob
 
 def search_string(directory, search_string,extension):
     for file in glob.glob(f'{directory}/**/*.{extension}'):
-        print(file)
+        # print(file)
         with open(file, "r") as f:
             content = f.read()
             if search_string in content:
                 print(f"Found '{search_string}' in {file}")
                 print(content.index(search_string))
 
-search_string(dirpath, 'index', 'txt')
+# search_string(dirpath, 'test', 'txt')
+
+
+def search_string3(directory, search_string,extension):
+    for file in glob.glob(f'{directory}/**/*.{extension}'):
+        with open(file, 'r') as f:
+            content = f.readlines()
+            for i, line in enumerate(content):
+                if re.search(search_string, line):
+                    print(f"Found '{search_string}' in {file}")
+                    print(f'Line number:{i + 1}; Content: {line}'.strip())
+                    print()
+
+search_string3(dirpath, 'index', 'py')
 
