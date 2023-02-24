49ea1cd3cadc1ae8422ac29c935ceace8b36b4f4
data-manipulation-process <jagadusu01@gmail.com>
2023-02-24 00:20:16 +0000

    search string and print with line number

--- class-12/fp.py
+++ class-12/fp.py
@@ -28,5 +28,29 @@ def search_string3(directory, search_string,extension):
                     print(f'Line number:{i + 1}; Content: {line}'.strip())
                     print()
 
-search_string3(dirpath, 'test', 'py')
+# search_string3(dirpath, 'test', 'py')
 
+
+
+def search_string4(directory, search_string,extension):
+    for file in glob.glob(f'{directory}/**/*.{extension}'):
+        # print(file)
+        with open(file, 'r') as f:
+            content = f.readlines()
+            # print(content)
+            for i, line in enumerate(content):
+                # print(i, line)
+                if re.search(search_string, line):
+                    print(f"Found '{search_string}' in {file}")
+                    print(f'Line number:{i + 1}; Content: {line}'.strip())
+                    print()
+
+search_string4(dirpath, 'test', 'py')
+
+
+"""
+get only files need by - filter
+read file
+line number, read file
+reg search - print line number with line content
+"""
