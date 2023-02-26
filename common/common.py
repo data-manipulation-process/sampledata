4afcec54c9e4c1904dfff5c260eaabdc03b9fb0e
data-manipulation-process <jagadusu01@gmail.com>
2023-02-26 20:39:20 +0000

    common file updated

--- common/common.py
+++ common/common.py
@@ -1,9 +1,9 @@
-
 import os
 
 fRootPath = "/Users/developer1/workspace/sampledata/class-1"
 fFolder = "customer"
 fFileName = "customer.txt"
+
 
 def create_dir(path):
     if not os.path.isdir(path):
